8e6013e1641a26c676582ec4eb5aa91c6b73b7ff
unknown <sawyerjhood@gmail.com>
2016-05-10 15:39:45 -0400

    formatted shit correctly

--- Assets/SwingScript.cs
+++ Assets/SwingScript.cs
@@ -1,66 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SwingScript : MonoBehaviour {
+public class SwingScript : MonoBehaviour
+{
 
     const int PLAYER_LAYER = 8;
     const int ENEMY_LAYER = 9;
     const int GRAPPLE_LAYER = 11;
 
     public GameObject anchor;
-	public Color c1 = Color.yellow;
-	public Color c2 = Color.red;
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
     public float swingSpeed = 10;
     public Sprite jumping;
     public Sprite standing;
-	private HingeJoint2D joint;
-	private HingeJoint2D anchorJoint;
-	private LineRenderer rope;
+    private HingeJoint2D joint;
+    private HingeJoint2D anchorJoint;
+    private LineRenderer rope;
     private Collider2D feetCollider;
     private SpriteRenderer renderer;
-    private enum PlayerState {Standing, Jumping};
+    private enum PlayerState { Standing, Jumping };
     private PlayerState playerState = PlayerState.Jumping;
 
-	void Start () {
-		rope = this.GetComponent<LineRenderer> ();
-		rope.material = new Material(Shader.Find("Particles/Additive"));
-		rope.SetColors(c1, c2);
-		rope.SetWidth(0.2F, 0.2F);
-		rope.SetVertexCount(2);
-		rope.enabled = false;
+    void Start()
+    {
+        rope = this.GetComponent<LineRenderer>();
+        rope.material = new Material(Shader.Find("Particles/Additive"));
+        rope.SetColors(c1, c2);
+        rope.SetWidth(0.2F, 0.2F);
+        rope.SetVertexCount(2);
+        rope.enabled = false;
         feetCollider = transform.Find("feet_collider").GetComponent<BoxCollider2D>();
         renderer = this.GetComponent<SpriteRenderer>();
-	}
+    }
 
-	void StartSwing(Vector3 pos) {
-		/** On left Click **/
-		var layerMask = 1 << GRAPPLE_LAYER;
-		Vector3 currentPos = gameObject.transform.position;
-		RaycastHit2D hit = Physics2D.Raycast (currentPos, pos - currentPos, Mathf.Infinity, layerMask);
-		if (hit.collider != null) {
-			Vector3 collPos = hit.point;
-			//move the anchor to the correct position
-			anchor.transform.position = new Vector3 (collPos.x, collPos.y, 0);
-			//zero out any rotation
-			anchor.transform.rotation = Quaternion.identity;
+    void StartSwing(Vector3 pos)
+    {
+        /** On left Click **/
+        var layerMask = 1 << GRAPPLE_LAYER;
+        Vector3 currentPos = gameObject.transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(currentPos, pos - currentPos, Mathf.Infinity, layerMask);
+        if (hit.collider != null)
+        {
+            Vector3 collPos = hit.point;
+            //move the anchor to the correct position
+            anchor.transform.position = new Vector3(collPos.x, collPos.y, 0);
+            //zero out any rotation
+            anchor.transform.rotation = Quaternion.identity;
 
-			//Create HingeJoints
-			joint = gameObject.AddComponent<HingeJoint2D> ();
-			joint.anchor = Vector3.zero;
-			joint.connectedBody = anchor.GetComponent<Rigidbody2D>();
-			anchorJoint = anchor.AddComponent<HingeJoint2D> ();
-			anchorJoint.anchor = Vector3.zero;
+            //Create HingeJoints
+            joint = gameObject.AddComponent<HingeJoint2D>();
+            joint.anchor = Vector3.zero;
+            joint.connectedBody = anchor.GetComponent<Rigidbody2D>();
+            anchorJoint = anchor.AddComponent<HingeJoint2D>();
+            anchorJoint.anchor = Vector3.zero;
 
-			rope.enabled = true;
-		}
-	}
+            rope.enabled = true;
+        }
+    }
 
-	void StopSwing () {
-		//Destroy HingeJoints
-		rope.enabled = false;
-		Destroy (joint);
-		Destroy (anchorJoint);
-	}
+    void StopSwing()
+    {
+        //Destroy HingeJoints
+        rope.enabled = false;
+        Destroy(joint);
+        Destroy(anchorJoint);
+    }
 
     bool IsRopeCut()
     {
@@ -72,10 +77,12 @@ public class SwingScript : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVector, dirVector.magnitude, 1 << ENEMY_LAYER);
         return hit.collider != null;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             if (playerState == PlayerState.Standing)
             {
                 gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 300);
@@ -83,16 +90,20 @@ public class SwingScript : MonoBehaviour {
             else {
                 StartSwing(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             }
-        } else if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || IsRopeCut()) {
-            StopSwing();
-        } 
-        if (rope.enabled) {
-			rope.SetPosition (0, transform.position);
-			rope.SetPosition (1, anchor.transform.position);
         }
-	}
+        else if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || IsRopeCut())
+        {
+            StopSwing();
+        }
+        if (rope.enabled)
+        {
+            rope.SetPosition(0, transform.position);
+            rope.SetPosition(1, anchor.transform.position);
+        }
+    }
 
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
         if (rope.enabled)
         {
             if (Input.GetKey(KeyCode.D))
